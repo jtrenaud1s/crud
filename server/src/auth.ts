0b0f3c4b2b15ba20d6dc1dadf654b1df2f148c4b
jtrenaud1s <jtrenaud1s@semo.edu>
2021-08-01 05:20:01 -0500

    save progress

--- server/src/auth.ts
+++ server/src/auth.ts
@@ -1,19 +1,23 @@
 import { sign } from "jsonwebtoken";
 import User from "./entities/User";
-import {Response} from 'express'
+import { Response } from "express";
 
 export const createAccessToken = (user: User) => {
-    return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
-        expiresIn: "15m",
-      })
-}
+  return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, {
+    expiresIn: "15m",
+  });
+};
 
 export const createRefreshToken = (user: User) => {
-    return sign({ userId: user.id, tokenVersion: user.tokenVersion }, process.env.REFRESH_TOKEN_SECRET!, {
-        expiresIn: "7d",
-      })
-}
+  return sign(
+    { userId: user.id, tokenVersion: user.tokenVersion },
+    process.env.REFRESH_TOKEN_SECRET!,
+    {
+      expiresIn: "7d",
+    }
+  );
+};
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie("boog", token, {httpOnly: true, path: "/refresh_token"})
-}
+  res.cookie("boog", token, { httpOnly: true, path: "/refresh_token" });
+};
